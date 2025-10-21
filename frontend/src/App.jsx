971b1e13b04cd2b271a0fa60ee50cd6c5512e687
Mr-Prakash6381 <prakash875478@gmail.com>
2025-10-21 08:57:38 +0530

    feat : add the new feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='flex flex-row '>
+      <button className='border rounded px-3 py-1 bg-green-600 text-white font-serif text-md'>Add Reconded</button>
+      <table className='text-center'>
+        <thead>
+          <tr>
+            <th className='border px-2 py-1 bg-gray-300 '>So. No</th>
+            <th className='border px-2 py-1 bg-gray-300 '>Reg Number</th>
+            <th className='border px-2 py-1 bg-gray-300 '>Name</th>
+            <th className='border px-2 py-1 bg-gray-300 '>Course</th>
+            <th className='border px-2 py-1 bg-gray-300 '>Department</th>
+            <th className='border px-2 py-1 bg-gray-300 '>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className='border px-2 py-1'>1</td>
+            <td className='border px-2 py-1'>101</td>
+            <td className='border px-2 py-1'>PrakashS</td>
+            <td className='border px-2 py-1'>M.sc</td>
+            <td className='border px-2 py-1'>Computer Science</td>
+            <td className='border px-2 py-1'>
+              <button className='border px-2 py-1 mx-2 rounded  '>Edit</button>
+              <button className='border px-2 py-1 mx-2 rounded  '>Delect</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
